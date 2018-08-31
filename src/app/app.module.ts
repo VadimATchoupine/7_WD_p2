985907f6222cefc00a634b2f58eec8931d81eb94
VadimATchoupine <vadim.tchoupine@gmail.com>
2018-08-31 22:29:15 +0300

    updated menu component p2 and added card element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 
 import { MatToolbarModule } from '@angular/material/toolbar'; // 2. Imported Material Toolbar Module
 import { MatListModule } from '@angular/material/list'; // 6. Imported Material List Module to use it in app
+import { MatGridListModule } from '@angular/material/grid-list';  // 8. Imported Material Grid List Module
+import { MatCardModule } from '@angular/material/card'; // 9. Imported Material Card Module
+import { MatButtonModule } from '@angular/material/button'; // 10. Imported MAterial Button Module
 import { FlexLayoutModule } from '@angular/flex-layout'; // 3. Imported Flex-Layout Module
 
 
@@ -22,7 +25,10 @@ import { MenuComponent } from './menu/menu.component';
     BrowserAnimationsModule,  // 5.1
     MatToolbarModule,         // 5.2
     FlexLayoutModule,         // 5.3
-    MatListModule             // 7 updated JS object with MatList Module for my menu
+    MatListModule,            // 7 updated JS object with MatList Module for my menu
+    MatGridListModule,        // 11
+    MatCardModule,            // 12
+    MatButtonModule           // 13
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,6 +2,50 @@ import { Component, OnInit } from '@angular/core';
 
 import { Dish } from '../shared/dish'; // 1. Imported created previously importable class Dish
 
+/* ---------------------------------------------------------- */
+// 2. TEMP - create a array with JS objects related to previously created TS class Dish
+// (should be added for services a little bit later)
+const DISHES: Dish[] = [
+  // 1st/4 JS object
+  {
+    name: 'Uthappizza',
+    image: '/assets/images/uthappizza.png',
+    category: 'mains',
+    label: 'Hot',
+    price: '4.99',
+    // tslint:disable-next-line:max-line-length // WTF??????
+    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+  },
+  // 2nd/4 JS object
+  {
+    name: 'Zucchipakoda',
+    image: '/assets/images/zucchipakoda.png',
+    category: 'appetizer',
+    label: '',
+    price: '1.99',
+    description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
+  },
+  // 3rd/4 JS object
+  {
+    name: 'Vadonut',
+    image: '/assets/images/vadonut.png',
+    category: 'appetizer',
+    label: 'New',
+    price: '1.99',
+    description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
+  },
+  // 4th/4 JS object
+  {
+    name: 'ElaiCheese Cake',
+    image: '/assets/images/elaicheesecake.png',
+    category: 'dessert',
+    label: '',
+    price: '2.99',
+    description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
+  }
+];
+/* ---------------------------------------------------------- */
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -9,48 +53,10 @@ import { Dish } from '../shared/dish'; // 1. Imported created previously importa
 })
 export class MenuComponent implements OnInit {
 
-  /* ---------------------------------------------------------- */
-  // 2. TEMP - create a array with JS objects related to previously created TS class Dish
-  dishes: Dish[] = [
-    // 1st/4 JS object
-    {
-      name: 'Uthappizza',
-      image: '/assets/images/uthappizza.png',
-      category: 'mains',
-      label: 'Hot',
-      price: '4.99',
-      // tslint:disable-next-line:max-line-length // WTF??????
-      description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
-    },
-    // 2nd/4 JS object
-    {
-      name: 'Zucchipakoda',
-      image: '/assets/images/zucchipakoda.png',
-      category: 'appetizer',
-      label: '',
-      price: '1.99',
-      description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
-    },
-    // 3rd/4 JS object
-    {
-      name: 'Vadonut',
-      image: '/assets/images/vadonut.png',
-      category: 'appetizer',
-      label: 'New',
-      price: '1.99',
-      description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
-    },
-    // 4th/4 JS object
-    {
-      name: 'ElaiCheese Cake',
-      image: '/assets/images/elaicheesecake.png',
-      category: 'dessert',
-      label: '',
-      price: '2.99',
-      description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
-    }
-  ];
-  /* ---------------------------------------------------------- */
+  // dishes = DISHES; // 3.a TypeScript will get it what type are we using there, but It would be more clear for me to use another approach 
+  dishes: Dish[] = DISHES; // 3.b
+
+  selectedDish: Dish = DISHES[0]; // 4. or selectedDish = DISHES[0];
 
   constructor() { }
 
